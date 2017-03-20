6278cb67f3d89d5ed76dc459b472b0270625281e
Ian Fisk <i.fisk32@gmail.com>
2017-03-20 01:36:43 -0700

    Fade in the question.

--- src/question/index.jsx
+++ src/question/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import { Motion, spring } from 'react-motion';
 import { styles } from './styles';
 
 export default class Question extends Component {
@@ -6,12 +7,44 @@ export default class Question extends Component {
 		text: PropTypes.string.isRequired,
 	};
 
+	state = {
+		fadeQuestionIn: false,
+	};
+
+	componentDidMount() {
+		setTimeout(() => this.setState({fadeQuestionIn: true}), 600);
+	}
+
+	getDefaultStyle = () => {
+		return {
+			opacity: 0,
+			left: -30,
+			top: 5,
+		};
+	}
+
+	getStyle = prevInterpolatedStyles => {
+		return {
+			opacity: spring(1),
+			left: spring(0),
+			top: spring(0),
+		};
+	};
+
   render() {
+		const { fadeQuestionIn } = this.state;
 		const { text } = this.props;
 
     return (
       <div style={styles.container}>
-				<span style={{alignSelf: 'flex-start'}}>{text}</span>
+				{fadeQuestionIn ?
+					<Motion
+						defaultStyle={this.getDefaultStyle()}
+						style={this.getStyle()}>
+						{style => <span style={{...style, alignSelf: 'flex-start', position: 'relative'}}>{text}</span>}
+					</Motion> :
+					null
+				}
       </div>
     );
   }
